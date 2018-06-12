0d212e6b732e7880d66d6cada61eea6c380188c3
Tate <tatesmith02@gmail.com>
2018-06-12 03:07:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,33 @@ function subtract(a, b){
   return a -= b;
 }
 subtract(23,12);
+
+function add(a, b){
+  return a += b;
+}
+add(45, 32);
+
+function multiply(a, b){
+  return a *= b;
+}
+multiply(67, 82);
+
+function divide(a, b){
+  return a /= b;
+}
+divide(50, 54);
+
+function inc(n){
+  return n++;
+}
+inc(9);
+
+function dec(n){
+  return n--;
+}
+dec(-4);
+
+function makeInt(n){
+  return parseInt(n);
+}
+makeInt('10');
